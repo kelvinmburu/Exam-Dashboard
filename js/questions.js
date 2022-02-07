5b455d47c41c9518c788523e53b565a0ace40566
kelvinmburu <mburuhkelvin@gmail.com>
2022-02-07 18:23:32 +0300

    js: feat - adding functionality to the questions

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// Adding a js arra for the questions, answers, and numbers will
+
+let questions = [
+    {
+        numb: 1,
+        question: "True or false: Javascript is a programming language that adds functionality to a web page.",
+        answer: "True",
+        options: [
+            "True",
+            "False",
+            "Neither",
+            "I don't know",
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which of the following isn't a math operator in Javascript?",
+        answer: "AND(&&)",
+        options: [
+            "Add(+)",
+            "Subtract(-)",
+            "Multiply(*)",
+            "AND(&&)",
+        ]
+    },
+    {
+        numb: 3,
+        question: "True or false: .concat() is used merge two or more arrays together?",
+        answer: "True",
+        options: [
+            "True",
+            "False",
+            "Neither",
+            "I don't know",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What forms the basis of web layout and wraps around every HTML element?",
+        answer: "CSS Box Model",
+        options: [
+            "CSS Box Model",
+            "CSS padding",
+            "CSS Flexbox",
+            "CSS Margin",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following is not a Javascript datatype",
+        answer: "No",
+        options: [
+            "String",
+            "Number",
+            "No",
+            "Boolean",
+        ]
+    },
+]
