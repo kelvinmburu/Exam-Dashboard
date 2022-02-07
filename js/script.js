f04b8cee5556c41b71e8ba399a5b1234744d0d45
kelvinmburu <mburuhkelvin@gmail.com>
2022-02-07 17:53:44 +0300

    feat: javascript functionality - adding functionality to start and exit button

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Hello world")
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = document.querySelector(".buttons .quit");
+const continue_btn = document.querySelector(".buttons .restart");
+
+// What happens when start button is clicked. It should show the info box
+start_btn.onclick = () => {
+    info_box.classList.add("activeInfo");
+}
+
+// What happens when exit button is clicked. This hides the info box
+exit_btn.onclick = () => {
+    info_box.classList.remove("activeInfo");
+}
